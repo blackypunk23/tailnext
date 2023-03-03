d51047aec1004da1add24f03c2a2c0cd094365c0
blackypunk23 <blackypunk23@gmail.com>
2023-03-03 15:40:26 -0600

    a
    a

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: colors.blue,
+        primary: colors.green,
         secondary: colors.blue,
       },
       fontFamily: {
